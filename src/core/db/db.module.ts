4273a2b3364a2a35cc198c7f522141005e22919a
Aditya Singh <aditya.singh@binmile.com>
2025-04-09 17:26:31 +0530

    env updated

--- src/core/db/db.module.ts
+++ src/core/db/db.module.ts
@@ -1,17 +1,19 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/apis/user/entities/user.entity';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Global()
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'adityasingh393',
-      password: 'Aditya@20',
-      database: 'tw-auth',
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       // entities: [__dirname + '/**/*.entity{.ts,.js}'],
       entities: [User],
       migrations: [__dirname + '/migrations/*.ts'],
