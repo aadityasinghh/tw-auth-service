1fabcbb946e8e28c050c47c5fbe97fe9028d6196
Aditya Singh <aditya.singh@binmile.com>
2025-05-08 14:40:15 +0530

    lint error fixes

--- src/apis/user/dto/email-verification.dto.ts
+++ src/apis/user/dto/email-verification.dto.ts
@@ -1,9 +1,4 @@
-import {
-    IsEmail,
-    IsNotEmpty,
-    IsString,
-    Length,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class RegisterUserDto {
     @IsString()
